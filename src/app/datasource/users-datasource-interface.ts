38b5e0fd786c3cadec7e648c16134f441ce0481a
vika_pavlenko_161105 <vika.pavlenko.161105@gmail.com>
2024-05-28 10:34:47 +0300

    Uncommented methods in datasourse-interface.

--- src/app/datasource/users-datasource-interface.ts
+++ src/app/datasource/users-datasource-interface.ts
@@ -3,9 +3,9 @@ import { UserModel } from "../models/user.model";
 export interface UsersDatasourceInterface {
     getUsersAsync(): Promise<UserModel[]>;
 
-    addUserAsync(newUser: UserModel): Promise<UserModel>;
+    // addUserAsync(newUser: UserModel): Promise<UserModel>;
 
-    updateUserAsync(user: UserModel): Promise<UserModel>;
+    // updateUserAsync(user: UserModel): Promise<UserModel>;
 
-    deleteUserAsync(user: UserModel): Promise<boolean>;
+    // deleteUserAsync(user: UserModel): Promise<boolean>;
 }
